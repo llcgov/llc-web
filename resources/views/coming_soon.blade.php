e5a05c7279049e8ae75f783efe934945de8afd30
Brisco <brix.does@gmail.com>
2020-02-13 16:49:32 +0800

    created one lapu lapu logo footer

--- resources/views/coming_soon.blade.php
+++ resources/views/coming_soon.blade.php
@@ -12,12 +12,6 @@
   <link rel="stylesheet" type="text/css" href="css/coming/css/animate.css">
   <link rel="stylesheet" type="text/css" href="css/coming/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="css/coming/css/style.css">
-  <!-- =======================================================
-    Theme Name: Maundy
-    Theme URL: https://bootstrapmade.com/maundy-free-coming-soon-bootstrap-theme/
-    Author: BootstrapMade.com
-    Author URL: https://bootstrapmade.com
-  ======================================================= -->
 </head>
 
 <body>
@@ -29,14 +23,6 @@
           <h2>Coming Soon</h2>
         </div>
         <div id="countdown" data-wow-delay=".3s" data-date="Mar 30, 2020 06:00:00"></div>
-        {{-- <h2 class="subs-title text-center">Subscribe now to get Recent updates!!!</h2>
-        <div class="subcription-info text-center">
-          <form class="subscribe_form" action="#" method="post">
-            <input required="" value="" placeholder="Enter your email..." class="email" id="email" name="email" type="email">
-            <input class="subscribe" name="email" value="Subscribe!" type="submit">
-          </form>
-          <p class="sub-p">We Promise to never spam you.</p>
-        </div> --}}
       </div>
     </div>
     <section>
@@ -51,115 +37,32 @@
         </div>
         <h4>Follow us on our facebook pages: </h4>
           
-              <div class="row">
-                <div class="col-md-6">
-                    <div class="social bg-dark text-light">
-                      <a href="https://www.facebook.com/Junard-Ahong-Chan-1597892066938019/">
-                        <span><h4 style="color:white"><i class="fa fa-facebook"></i>Junard "Ahong" Chan</h4></span>
-                      </a>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                  <div class="social bg-dark">
-                    <a href="https://www.facebook.com/lapulapucitygovernment/">
-                      <span class="text-decoration-none"><h4 style="color:white"><i class="fa fa-facebook"></i>Lapu-Lapu City Government - Chan Administration</h4></span>
-                    </a>
-                    </a>
+            <div class="row">
+              <div class="col-md-4">
+                <div class="social">
+                  <a href="https://www.facebook.com/lapulapucitygovernment/">
+                    <img src="{{ asset('/img/onelapulapu.png') }}" width="250px" alt="">
+                  </a>
                 </div>
               </div>
+
+              <div class="col-md-4">
+                <div class="social">
+                  <a href="https://www.facebook.com/lapulapucitygovernment/">
+                    <img src="{{ asset('/img/onelapulapu.png') }}" width="250px" alt="">
+                  </a>
+                </div>
               </div>
-         
-        {{-- <div class="row bort text-center">
-          <div class="social">
-            
-          </div>
-        </div> --}}
-      </div>
-    </section>
-    
-{{-- <div class="container bg-dark">
-  <section id="about" class="section-padding">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12 col-sm-12 text-center">
-          <div class="about-title">
-            <h2>About Us</h2>
-            <p>Lapu-Lapu City, officially the City of Lapu-Lapu or simply known as Lapu-Lapu, is a 1st class highly urbanised city in the region of Central Visayas,Philippines.According to the 2015 census, it has a population of 408,112 people
-              </br>Formerly known as Opon, the city was renamed to its present name in 1961. It is one of the cities that make up Metro Cebu in the Philippines. It is geographically located in the province of Cebu, administratively independent from the province, but grouped under Cebu by the Philippine Statistics Authority (PSA). Lapu-Lapu City is bounded on the north by the main island of Cebu, to the west by Cebu City and Mactan Channel, on the east by the Camotes Sea, and on the south by the town of Cordova.
-               The city occupies Mactan Island, a few kilometers off the main island of Cebu. It also has some of the barangays under its jurisdiction on the Olango Island Group. The city is linked to Mandaue on mainland Cebu by the Mactan-Mandaue Bridge and Marcelo Fernan Bridges. Mactan-Cebu International Airport, the second busiest airport in the Philippines, is located in Lapu-Lapu. </p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-</div> --}}
-{{--
-<div class="container bg-dark">
-  <div id="contact-info">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6">
-          <div class="contact-title">
-            <i class="fa fa-envelope"></i>
-            <h2>Get in touch</h2>
-            <p> <br>incididunt ut labore et dolore magna aliqua.</p>
-          </div>
-        </div>
-        <div class="contact col-md-6 wow fadeIn delay-08s bg-dark-form">
-          <div class="col-md-10 col-md-offset-1">
-            <div id="note"></div>
-            <div id="sendmessage">Your message has been sent. Thank you!</div>
-            <div id="errormessage"></div>
-            <form action="" method="post" role="form" class="contactForm">
-              <div class="form-group">
-                <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                <div class="validation"></div>
+                
+              <div class="col-md-4">
+                <div class="social">
+                  <a href="https://www.facebook.com/lapulapucitygovernment/">
+                    <img src="{{ asset('/img/onelapulapu.png') }}" width="250px" alt="">
+                  </a>
+                </div>
               </div>
-              <div class="form-group">
-                <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                <div class="validation"></div>
-              </div>
-              <div class="form-group">
-                <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
-                <div class="validation"></div>
-              </div>
-              <div class="form-group">
-                <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
-                <div class="validation"></div>
-              </div>
-              <div class="text-center"><button type="submit" class="contact-submit">Send Message</button></div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
+            </div>
   </div>
-
-</div>
---}}
-
-  </div>
-  {{--
-  <footer class="footer">
-    <div class="container">
-      <div class="row bort">
-
-        <div class="copyright">
-          © Copyright Maundy Theme. All rights reserved.
-          <div class="credits">
-            <!--
-              All the links in the footer should remain intact. 
-              You can delete the links only if you purchased the pro version.
-              Licensing information: https://bootstrapmade.com/license/
-              Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Maundy
-            -->
-          </div>
-        </div>
-
-      </div>
-    </div>
-  </footer>
-  --}}
   <script src="css/coming/js/jquery.min.js"></script>
   <script src="css/coming/js/bootstrap.min.js"></script>
   <script src="css/coming/js/jquery.countdown.min.js"></script>
