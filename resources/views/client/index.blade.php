1697f04481a94de7bc64c96c629e5f347080ec56
Brisco <brix.does@gmail.com>
2020-02-20 15:32:27 +0800

    mommy gusto ko barbie

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -133,6 +133,81 @@
 
             <div class="row my-2">
                 <div class="col-md-6"> 
+                    <div class="card text-white bg-danger">
+                        <div class="card-header">
+                            <h5 class="font-weight-bold">LATEST NEWS</h5>
+                        </div>
+                        <ul class="list-group list-group-flush scroll">
+                            @foreach ($posts as $item)
+                                @if ($item->post_type == 'Latest Posts')
+                                    <li class="text-dark list-group-item"> 
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <img src="{{ url( 'storage/' . $item->media()->first()->path . '/' . $item->media()->first()->name) }}" alt="" width="80px">
+                                            </div>
+                                            <div class="col-md-8">
+                                                <span class="text-uppercase font-content"><p> {{ $item->title}}</p></span>
+                                                <span class="text-uppercase font-content small text-muted"> {{ date('d-m-Y', strtotime($item->created_at))}}</span>
+                                            </div>
+                                        </div>
+                                    </li>
+                                @endif
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card text-white bg-danger">
+                        <div class="card-header">
+                            <h5 class="font-weight-bold">LIBRENG SERBISYO</h5>
+                        </div>
+                        <ul class="list-group list-group-flush scroll">
+                            @foreach ($posts as $item)
+                                @if ($item->post_type == 'Latest Post')
+                                    <li class="text-dark list-group-item"> 
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <img src="{{ url( 'storage/' . $item->media()->first()->path . '/' . $item->media()->first()->name) }}" alt="" width="80px">
+                                            </div>
+                                            <div class="col-md-8">
+                                                <span class="text-uppercase font-content"><p> {{ $item->title}}</p></span>
+                                                <span class="text-uppercase font-content small text-muted"> {{ date('d-m-Y', strtotime($item->created_at))}}</span>
+                                            </div>
+                                        </div>
+                                    </li>
+                                @endif
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card text-white bg-danger">
+                        <div class="card-header">
+                            <h5 class="font-weight-bold">SPORTS</h5>
+                        </div>
+                        <ul class="list-group list-group-flush scroll">
+                            @foreach ($posts as $item)
+                                @if ($item->post_type == 'Latest Post')
+                                    <li class="text-dark list-group-item"> 
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <img src="{{ url( 'storage/' . $item->media()->first()->path . '/' . $item->media()->first()->name) }}" alt="" width="80px">
+                                            </div>
+                                            <div class="col-md-8">
+                                                <span class="text-uppercase font-content"><p> {{ $item->title}}</p></span>
+                                                <span class="text-uppercase font-content small text-muted"> {{ date('d-m-Y', strtotime($item->created_at))}}</span>
+                                            </div>
+                                        </div>
+                                    </li>
+                                @endif
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="row my-2">
+                <div class="col-md-6"> 
                     <div class="embed-responsive embed-responsive-16by9">
                         <iframe class="embed-responsive-item" src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Flapulapucitygovernment%2Fvideos%2F395809897803594%2F&show_text=0" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
                       </div>
--- resources/views/client/layout/menu.blade.php
+++ resources/views/client/layout/menu.blade.php
@@ -9,39 +9,49 @@
         <div class="row offset-md-4 col-md-6 offset-md-2">
             {{-- <div class=""> --}}
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                <a class="nav-link" href="#"><h6 class="font-weight-bold"> HOME</h6> <span class="sr-only">(current)</span></a></li>
-                <li class="nav-item active" >
-                <a class="nav-link" href="#">
-                    <h6 class="font-weight-bold"> 
-                    ONLINE SERVICES
-                    </h6> 
-                    <span class="sr-only">(current)</span>
-                </a>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <h4 class="font-weight-light text-light"> HOME</h4> 
+                        <span class="sr-only">(current)</span>
+                    </a>
                 </li>
-                <li class="nav-item active">
-                <a class="nav-link" href="#">
-                    <h6 class="font-weight-bold">  
-                    DEPARTMENTS
-                    </h6> 
-                    <span class="sr-only">(current)</span>
-                </a>
+                <li class="nav-item dropdown">
+                    <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                        <h4 class="font-weight-light text-light dropdown-toggle">  
+                            ONLINE SERVICES
+                        </h4></a>
+                    <div class="dropdown-menu">
+                      <a class="dropdown-item" href="#"><h5 class="font-weight-light">New/Renew Business Permit</h5></a>
+                      <a class="dropdown-item" href="#"><h5 class="font-weight-light">Local Civil Registry Services</h5></a>
+                      <a class="dropdown-item" href="#"><h5 class="font-weight-light">Cedula</h5></a>
+                      <a class="dropdown-item" href="#"><h5 class="font-weight-light">Tax Declaration</h5></a>
+                    </div>
+                  </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <h4 class="font-weight-light text-light">  
+                            DEPARTMENTS
+                        </h4> 
+                        <span class="sr-only">(current)</span>
+                    </a>
                 </li>
-                <li class="nav-item active" >
-                <a class="nav-link" href="#">
-                    <h6 class="font-weight-bold">  
-                    TRANSPARANCY
-                    </h6> 
-                    <span class="sr-only">(current)</span>
-                </a>
-                </li>
-                <li class="nav-item active" >
-                <a class="nav-link" href="#">
-                    <h6 class="font-weight-bold"> 
-                    CONTACT US
-                    </h6> 
-                    <span class="sr-only">(current)</span>
-                </a>
+                <li class="nav-item dropdown">
+                    <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="true">
+                        <h4 class="font-weight-light text-light dropdown-toggle">  
+                            TRANSPARENCY
+                        </h4></a>
+                    <div class="dropdown-menu">
+                      <a class="dropdown-item" href="#"><h5 class="font-weight-light">Full Disclosure Data</h5></a>
+                      <a class="dropdown-item" href="#"><h5 class="font-weight-light">Executive Order</h5></a>
+                      <a class="dropdown-item" href="#"><h5 class="font-weight-light">Citizen's Chart</h5></a>
+                    </div>
+                  </li>
+                <li class="nav-item" >
+                        <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="true">
+                            <h4 class="font-weight-light text-light">  
+                                CONTACT US
+                            </h4></a>
+                        <span class="sr-only">(current)</span>
                 </li>
             </ul>
             {{-- </div> --}}
