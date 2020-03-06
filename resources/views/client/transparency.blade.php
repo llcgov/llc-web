bec63b2b713a3abef60796c5b2af4c1a769bae1c
Brisco <brix.does@gmail.com>
2020-03-06 11:15:57 +0800

    added feature transparency organize

--- resources/views/client/transparency.blade.php
+++ resources/views/client/transparency.blade.php
@@ -37,8 +37,26 @@
                                               <ul>
                                                 @foreach ($quart as $fdpdocs)
                                                 <li>
-                                                  {{ $fdpdocs['title'] }}
-                                                </li>
+                                                    <a href="" class="text-decoration-none" data-toggle="modal" data-target="#{{ str_replace(' ', '', $fdpdocs->title)  }}"><h5 class="text-uppercase">{{ $fdpdocs->title }}</h5></a>
+                                                  </li>                          
+                                                    {{-- MODAL --}}
+                                                    <div class="modal fade bd-example-modal-xl" id="{{ str_replace(' ', '', $fdpdocs->title) }}" tabindex="-1" role="dialog" aria-labelledby="{{ $fdpdocs->id }}" aria-hidden="true">
+                                                      <div class="modal-dialog modal-xl" role="document">
+                                                        <div class="modal-content">
+                                                          <div class="modal-header">
+                                                            <h5 class="modal-title" id="{{ $fdpdocs->title }}">{{ $fdpdocs->title }}</h5>
+                                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                              <span aria-hidden="true">&times;</span>
+                                                            </button>
+                                                          </div>
+                                                          <div class="modal-body">
+                                                              <div class="embed-responsive embed-responsive-16by9">
+                                                                  <iframe class="embed-responsive-item" src="{{ url( 'storage/PDF/' . $fdpdocs->name) }}" frameborder="0"></iframe>
+                                                              </div>
+                                                          </div>
+                                                        </div>
+                                                      </div>
+                                                    </div>
                                                 @endforeach
                                               </ul>
                                     </div>
