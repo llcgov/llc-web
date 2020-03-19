7952a4abaaf95e782251a5e6fe3c2a523fe641d0
Brisco <brix.does@gmail.com>
2020-03-19 08:33:58 +0800

    added new tawk.to chat

--- resources/views/client/layout/master.blade.php
+++ resources/views/client/layout/master.blade.php
@@ -51,6 +51,19 @@
           </div>
         </div>
         <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+        <!--Start of Tawk.to Script-->
+        <script type="text/javascript">
+          var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+          (function(){
+          var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+          s1.async=true;
+          s1.src='https://embed.tawk.to/5e72136e8d24fc2265885a98/default';
+          s1.charset='UTF-8';
+          s1.setAttribute('crossorigin','*');
+          s0.parentNode.insertBefore(s1,s0);
+          })();
+        </script>
+          <!--End of Tawk.to Script-->
         @stack('js')
       </body>
 </html>
