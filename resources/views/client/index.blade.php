4f47a4465cd2986e77c72c2bd4039d2d4dd618ed
Brisco <brix.does@gmail.com>
2020-03-20 17:11:06 +0800

    redesign index

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -29,7 +29,7 @@
     <div class="row posts">
         <div class="offset-md-2 col-md-8">
             <div class="row rounded py-3">
-                <div class="col-md-3">
+                {{-- <div class="col-md-3">
                     <div class="card text-white bg-danger">
                         <div class="card-header">
                             <h5 class="font-weight-bold font-responsive">EVENTS</h5>
@@ -55,8 +55,8 @@
                             @endforeach
                         </ul>
                     </div>
-                </div>
-                <div class="col-md-3">
+                </div> --}}
+                <div class="col-md-4">
                     <div class="card text-white bg-success">
                         <div class="card-header">
                             <h5 class="font-weight-bold font-responsive">TOURISM</h5>
@@ -83,7 +83,7 @@
                         </ul>
                     </div>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <div class="card text-white bg-success">
                         <div class="card-header">
                             <h5 class="font-weight-bold font-responsive">BIDS AND AWARDS</h5>
@@ -110,7 +110,7 @@
                         </ul>
                     </div>
                 </div>
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <div class="card text-white bg-success">
                         <div class="card-header">
                             <h5 class="font-weight-bold font-responsive">JOBS</h5>
@@ -138,8 +138,7 @@
                     </div>
                 </div>
             </div>
-
-            <div class="row my-2 rounded">
+            <div class="row my-2 rounded py-3">
                 <div class="col-md-6"> 
                     <div class="card text-white bg-danger">
                         <div class="card-header">
@@ -165,6 +164,57 @@
                                 @endif
                             @endforeach
                         </ul>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item"  src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2F1597892066938019%2Fvideos%2F251789805842179%2F&show_text=0" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="offset-md-2 col-md-8">
+
+            <div class="row my-2  bg-white rounded py-3">
+                <div class="col-md-3">
+                    <div class="card text-white bg-danger">
+                        <div class="card-header">
+                            <h5 class="font-weight-bold font-responsive">EVENTS</h5>
+                        </div>
+                        <ul class="list-group list-group-flush scroll-lg">
+                            @foreach ($posts as $item)
+                                @if ($item->post_type == 'Events')
+                                <a href="{{ route('post.show', $item->id) }}" class="text-decoration-none">
+
+                                    <li class="text-dark list-group-item post"> 
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <img class="img-thumbnail" src="{{ !empty($item->media()->first()->path) ? url( 'storage/' . $item->media()->first()->path . '/' . $item->media()->first()->name) : '' }}" alt="">
+                                            </div>
+                                            <div class="col-md-8">
+                                                <span class="font-content"><p> {{ $item->title}}</p></span>
+                                                <span class="text-uppercase font-content small text-muted"> {{ date('d-m-Y', strtotime($item->created_at))}}</span>
+                                            </div>                                            
+                                        </div>
+                                    </li>
+                                </a>
+                                @endif
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-9">
+                    <div id='calendar'></div>
+                </div>
+            </div>
+            <div class="row my-2 bg-white rounded">
+                <div class="col-md-6"> 
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item" src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Flapulapucitygovernment%2Fvideos%2F395809897803594%2F&show_text=0" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
                     </div>
                 </div>
                 <div class="col-md-3">
@@ -224,22 +274,6 @@
             </div>
         </div>
     </div>
-    
-
-    <div class="row">
-        <div class="offset-md-2 col-md-8">
-            <div class="row my-2  bg-white rounded py-3">
-                <div class="col-md-6"> 
-                    <div class="embed-responsive embed-responsive-16by9">
-                        <iframe class="embed-responsive-item" src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Flapulapucitygovernment%2Fvideos%2F395809897803594%2F&show_text=0" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
-                        </div>
-                </div>
-                <div class="col-md-6">
-                    <div id='calendar'></div>
-                </div>
-            </div>
-        </div>
-    </div>
 
 </div>
 
@@ -251,7 +285,6 @@
               var calendar = new FullCalendar.Calendar(calendarEl, {
                 plugins: [ 'dayGrid' ],
                 defaultView: 'dayGridMonth',
-                // defaultDate: '2020-02-07',
                 defaultDate: new Date(),
                 header: {
                   left: 'prev, next',
