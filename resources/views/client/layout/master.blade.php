49178c0f2eaef64dde6f8ac1d6eb4168af8190e4
Brisco <brix.does@gmail.com>
2020-03-25 15:28:33 +0800

    enabled chat

--- resources/views/client/layout/master.blade.php
+++ resources/views/client/layout/master.blade.php
@@ -51,8 +51,8 @@
           </div>
         </div>
         <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
-        <!--Start of Tawk.to Script-->
-          {{-- <script type="text/javascript">
+        
+          <script type="text/javascript">
             var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
             (function(){
             var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
@@ -62,8 +62,8 @@
             s1.setAttribute('crossorigin','*');
             s0.parentNode.insertBefore(s1,s0);
             })();
-          </script> --}}
-        <!--End of Tawk.to Script-->
+          </script>
+
         @stack('js')
       </body>
 </html>
