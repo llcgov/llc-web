77cee3a680ba7621c70a195c7006fe5a56ede16d
Brisco <brix.does@gmail.com>
2020-04-09 14:36:21 +0800

    edit article mod

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,7 +7,7 @@ use App\Models\Media;
 
 class Post extends Model
 {
-    protected $fillable = ['title', 'content', 'post_type', 'date_posted'];
+    protected $fillable = ['title', 'content', 'post_type', 'date_posted', 'headline', 'video_url'];
     
     public function media(){
         return $this->morphToMany(Media::class, 'mediable');
