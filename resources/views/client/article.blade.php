85d9916ec7a5e8bbbe71dfc4c6f8da3ebc049094
Brisco <brix.does@gmail.com>
2020-04-09 08:35:48 +0800

    article division

--- resources/views/client/article.blade.php
+++ resources/views/client/article.blade.php
@@ -3,14 +3,28 @@
 @section('content')
     <div class="container">
         <div class="row m-5">
-            <div class="col-md-6">
-                <img class="img-thumbnail" src="{{ !empty($post->media()->first()->path) ? url( 'storage/' . $post->media()->first()->path . '/' . $post->media()->first()->name) : '' }}" alt="">
-            </div>
-            <div class="col-md-6">
+            @if (!empty($post->media()->first()->path))
+                <div class="col-md-6">
+                    <img class="img-thumbnail" src="{{ !empty($post->media()->first()->path) ? url( 'storage/' . $post->media()->first()->path . '/' . $post->media()->first()->name) : '' }}" alt="">
+                </div>
+                <div class="col-md-6">
+                    <span class="font-content"><p> {{ $post->title}}</p></span>
+                    <span class="font-content"><p> {{ $post->content}}</p></span>
+                    <span class="text-uppercase font-content small text-muted"> {{ date('d-m-Y', strtotime($post->created_at))}}</span>
+                </div>
+            @else
+                <div class="col-md-12">
+                    <span class="font-content"><p> {{ $post->title}}</p></span>
+                    <span class="font-content"><p> {{ $post->content}}</p></span>
+                    <span class="text-uppercase font-content small text-muted"> {{ date('d-m-Y', strtotime($post->created_at))}}</span>
+                </div>
+            @endif
+            
+            {{-- <div class="col-md-12">
                 <span class="font-content"><p> {{ $post->title}}</p></span>
                 <span class="font-content"><p> {{ $post->content}}</p></span>
                 <span class="text-uppercase font-content small text-muted"> {{ date('d-m-Y', strtotime($post->created_at))}}</span>
-            </div>                                            
+            </div>                                             --}}
         </div>
     </div>
 @endsection
