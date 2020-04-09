a2347703454a40685c8828fbc46b56d6424aaaff
Brisco <brix.does@gmail.com>
2020-04-09 08:45:56 +0800

    article justify

--- resources/views/client/article.blade.php
+++ resources/views/client/article.blade.php
@@ -9,13 +9,13 @@
                 </div>
                 <div class="col-md-6">
                     {{-- <span class="font-content"><p> {{ $post->title}}</p></span> --}}
-                    <span class="font-content"><p> {{ $post->content}}</p></span>
+                    <span class="font-content text-justify"><p> {{ $post->content}}</p></span>
                     <span class="text-uppercase font-content small text-muted"> {{ date('d-m-Y', strtotime($post->created_at))}}</span>
                 </div>
             @else
                 <div class="col-md-12">
                     {{-- <span class="font-content"><p> {{ $post->title}}</p></span> --}}
-                    <span class="font-content"><p> {{ $post->content}}</p></span>
+                    <span class="font-content text-justify"><p> {{ $post->content}}</p></span>
                     <span class="text-uppercase font-content small text-muted"> {{ date('d-m-Y', strtotime($post->created_at))}}</span>
                 </div>
             @endif
