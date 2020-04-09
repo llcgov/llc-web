32daeab491d079a1903e959b09db454971d7377c
Brisco <brix.does@gmail.com>
2020-04-09 13:07:17 +0800

    prod reps

--- resources/views/client/article.blade.php
+++ resources/views/client/article.blade.php
@@ -8,23 +8,16 @@
                     <img class="img-thumbnail" src="{{ !empty($post->media()->first()->path) ? url( 'storage/' . $post->media()->first()->path . '/' . $post->media()->first()->name) : '' }}" alt="">
                 </div>
                 <div class="col-md-6">
-                    {{-- <span class="font-content"><p> {{ $post->title}}</p></span> --}}
                     <span class="font-content text-justify"><p> {{ $post->content}}</p></span>
                     <span class="text-uppercase font-content small text-muted"> {{ date('d-m-Y', strtotime($post->created_at))}}</span>
                 </div>
             @else
                 <div class="col-md-12">
-                    {{-- <span class="font-content"><p> {{ $post->title}}</p></span> --}}
                     <span class="font-content text-justify"><p> {{ $post->content}}</p></span>
                     <span class="text-uppercase font-content small text-muted"> {{ date('d-m-Y', strtotime($post->created_at))}}</span>
                 </div>
             @endif
-            
-            {{-- <div class="col-md-12">
-                <span class="font-content"><p> {{ $post->title}}</p></span>
-                <span class="font-content"><p> {{ $post->content}}</p></span>
-                <span class="text-uppercase font-content small text-muted"> {{ date('d-m-Y', strtotime($post->created_at))}}</span>
-            </div>                                             --}}
+
         </div>
     </div>
 @endsection
