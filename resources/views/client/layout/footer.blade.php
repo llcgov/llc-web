305999b2ecfcbf820461a8377a268bc17ddb569a
Briscoto <brix.does@gmail.com>
2020-05-21 08:32:36 +0800

    footer changes

--- resources/views/client/layout/footer.blade.php
+++ resources/views/client/layout/footer.blade.php
@@ -1,99 +1,29 @@
-    <div class="bg-primary">
-        <div class="offset-md-2 col-md-8 offset-md-2 mt-4">
-            <div class="row">
+<div class="bg-primary">
+    <div class="offset-md-3 col-md-6 mt-4">
+            <div class="row text-light">
                 <div class="col-md-4">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <img src="{{ asset('img/pg-new.png') }}" alt="llc-logo.png" width="100%" class="my-4">
-                        </div>
-                        <div class="col-md-6 my-3">
-                            <a href="#" class="text-decoration-none text-light"><h6>HOME</h6></a>
-                            <a href="#" class="text-decoration-none text-light"><h6>ONLINE SERVICES</h6></a>
-                            <a href="#" class="text-decoration-none text-light"><h6>DEPARTMENTS</h6></a>
-                            <a href="#" class="text-decoration-none text-light"><h6>TRANSPARENCY</h6></a>
-                            <a href="#" class="text-decoration-none text-light"><h6>ABOUT LAPU-LAPU CITY</h6></a>
-                            <a href="#" class="text-decoration-none text-light"><h6>ABOUT THE MAYOR</h6></a>
-
-                        </div>
-                    </div>
+                    <img src="{{ asset('img/pg-new.png') }}" alt="llc-logo.png" width="75%">
                 </div>
-                <div class="col-md-3 text-center">
-                    <h4 class="text-white">Email us</h4>
-                    <form action="#">
-                        <div class="form-group">
-                            <input type="text" name="email" class="form-control" placeholder="Email">
-                        </div>
-                        <div class="form-group">
-                            <textarea name="" id="" cols="30" rows="2" class="form-control"></textarea>
-                        </div>
-                        <div class="form-group text-right">
-                            <button type="submit" class="btn btn-danger">Send</button>
-                        </div>
-                    </form>
+                <div class="col-md-3 mt-4">
+                    <a href="#" class="text-decoration-none text-light"><h6>HOME</h6></a>
+                    <a href="#" class="text-decoration-none text-light"><h6>ONLINE SERVICES</h6></a>
+                    <a href="#" class="text-decoration-none text-light"><h6>DEPARTMENTS</h6></a>
+                    <a href="#" class="text-decoration-none text-light"><h6>TRANSPARENCY</h6></a>
+                    <a href="#" class="text-decoration-none text-light"><h6>ABOUT LAPU-LAPU CITY</h6></a>
+                    <a href="#" class="text-decoration-none text-light"><h6>ABOUT THE MAYOR</h6></a>
                 </div>
-                    <div class="col-md-5 text-light">
-                        <h4 class="text-center"><strong>CONTACT INFORMATION</strong></h4>
-                        <div>
-                            <h5><strong>Hotline: </strong> (032)-340 2124</h5>
-                            
-                            <h5> <strong>Office Hours: </strong> 8:00 AM - 5:00 PM Monday to Friday</h5>
+                <div class="col-md-5 mt-4">
+                    <h4><strong>CONTACT INFORMATION</strong></h4>
+                    <div>
+                        <h5><strong>Hotline: </strong> (032)-340 2124</h5>
+                        
+                        <h5> <strong>Office Hours: </strong> 8:00 AM - 5:00 PM Monday to Friday</h5>
 
-                            <h5> <strong>Email Address: </strong> info@lapulapucity.gov.ph</h5>
-                        </div>
+                        <h5> <strong>Email Address: </strong> info@lapulapucity.gov.ph</h5>
                     </div>
                 </div>
             </div>
         </div>
-        {{-- <div class="bg-primary">
-            <div class="offset-md-2 col-md-8 offset-md-2 mt-4">
-                <div class="row">
-                    <div class="col-md-5">
-                        <div class="row">
-                            <div class="col-md-6">
-                                <img src="{{ asset('img/pg-new.png') }}" alt="llc-logo.png" width="100%" class="my-4">
-                            </div>
-                            <div class="col-md-6 my-3">
-                                <h5 class="text-light"> <strong> REPUBLIC OF THE PHILIPPINES </strong> </h5>
-                                <h5 class="text-light">All content is in the public domain unless otherwise stated.</h5>
-    
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3 text-center">
-                        <h4 class="text-white">Email us</h4>
-                        <form action="#">
-                            <div class="form-group">
-                                <input type="text" name="email" class="form-control" placeholder="Email">
-                            </div>
-                            <div class="form-group">
-                                <textarea name="" id="" cols="30" rows="2" class="form-control"></textarea>
-                            </div>
-                            <div class="form-group text-right">
-                                <button type="submit" class="btn btn-danger">Send</button>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="col-md-4 text-light">
-                        <h4 class="text-center"><strong>CONTACT INFORMATION</strong></h4>
-                        <div>
-                            <h5><strong>Hotline: </strong> (032)-341 6666</h5>
-                            
-                            , <h5><strong>Mobile: </strong> +6395 6546 586</h5>
-                            
-                            <h5> <strong>Office Hours: </strong> 8:00 AM - 5:00 PM Monday to Friday</h5>
-
-                            <h5> <strong>Email Address: </strong> info@lapulapucity.gov.ph</h5>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div> --}}
-    {{-- <footer class="sticky-footer foot">
-        <div class="container my-auto">
-            <div class="copyright text-center my-auto">
-            <span class="text-light font-weight-bold"><h5>Copyright &copy; Your Website 2019</h5></span>
-            </div>
-        </div>
-    </footer> --}}
+    </div>
 
     <script src="{{ asset('js/sb-admin.js') }}"></script>
