205a45500738e0a0c420e507cc414de49ef133b3
Brisco <brix.does@gmail.com>
2020-10-12 15:49:08 +0800

    created new tourism page

--- resources/views/client/tourism.blade.php
+++ resources/views/client/tourism.blade.php
@@ -2,52 +2,63 @@
 @section('content')
     @push('css')
         <style>
-            .full-width-image {
-                height: 300px;
-                background: url("your-image.jpg") no-repeat center center fixed;
-                background-size: cover;
+            video {
+                object-fit: cover;
+                z-index: -1;
+                position: relative;
+                max-width: 100%;
+                top: -250px;
+            }   
+            .countdown{
+                margin-top: -58vh;
+                padding: 11vh;
+            }
+
+            .countdown span {
+                text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.1);
+                font-size: 3rem;
+                margin-left: 0.8rem;
+            }
+
+            .countdown-circles {
+                text-transform: uppercase;
+                font-weight: bold;
+            }
+
+            .countdown-circles span {
+                width: 100px;
+                height: 100px;
+                border-radius: 50%;
+                background: rgba(255, 255, 255, 0.2);
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.1);
             }
         </style>
     @endpush
-    <div class="container">
-        <div class="full-width-image">
-            <div class="m-5 p-5 text-center">
-                <h3 id="countdown"></h3>
-            </div>
+    <div class="container-fluid m-0 p-0">
+        <video playsinline autoplay muted loop id="bgvid">
+            <source src="{{ asset('files/balikturismo.mp4')}}" type="video/mp4">
+        </video>
+        <div class="rounded text-white text-center">
+            <div id="clock-b" class="countdown countdown-circles d-flex flex-wrap justify-content-center pt-4"></div>
         </div>
 
     </div>
     @push('scripts')
-    <script>
-        // Set the date we're counting down to
-        var countDownDate = new Date("Oct 17, 2020 06:00:00").getTime();
-        
-        // Update the count down every 1 second
-        var x = setInterval(function() {
-        
-          // Get today's date and time
-          var now = new Date().getTime();
-            
-          // Find the distance between now and the count down date
-          var distance = countDownDate - now;
-            
-          // Time calculations for days, hours, minutes and seconds
-          var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-          var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-          var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-          var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-            
-          // Output the result in an element with id="demo"
-          document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-          + minutes + "m " + seconds + "s ";
-            
-          // If the count down is over, write some text 
-          if (distance < 0) {
-            clearInterval(x);
-            document.getElementById("countdown").innerHTML = "EXPIRED";
-          }
-        }, 1000);
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.countdown/2.2.0/jquery.countdown.min.js"></script>
+        <script>
+         $(function () {
+            $('#clock-b').countdown('2020/10/17').on('update.countdown', function(event) {
+                var $this = $(this).html(event.strftime(''
+                    + '<div class="m-2"><span class="h1 font-weight-bold">%D</span> <h4>DAY%!d<h4></div>'
+                    + '<div class="m-2"><span class="h1 font-weight-bold">%H</span> <h4>Hr</h4></div>'
+                    + '<div class="m-2"><span class="h1 font-weight-bold">%M</span> <h4>Min</h4></div>'
+                    + '<div class="m-2"><span class="h1 font-weight-bold">%S</span> <h4>Sec</h4></div>'));
+                });
+            });
         </script>
-        
     @endpush
+    
 @endsection
