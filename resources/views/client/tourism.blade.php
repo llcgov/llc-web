8470666ec2d3dffa3ca5b0960b5e74897e37cbdc
Brisco <brix.does@gmail.com>
2020-10-15 13:07:42 +0800

    balik turismo visits websites and facebook page

--- resources/views/client/tourism.blade.php
+++ resources/views/client/tourism.blade.php
@@ -2,6 +2,32 @@
 @section('content')
     @push('css')
         <style>
+            .overlay {
+                position: absolute;
+                bottom: 0;
+                left: 0;
+                right: 0;
+                background-color: rgba(113, 120, 122, 0.5);
+                overflow: hidden;
+                width: 0;
+                height: 100%;
+                transition: .5s ease;
+            }
+
+            .container2{
+                position: relative;
+                display: inline-block;
+            }
+
+            
+            .container2:hover .overlay {
+                width: 75%;
+            }
+
+            .p2:hover{
+                opacity: .6;
+            }
+            
             video {
                 object-fit: cover;
                 z-index: -1;
@@ -43,7 +69,7 @@
                 <video playsinline autoplay muted loop id="bgvid">
                     <source src="{{ asset('files/balikturismo.mp4')}}" type="video/mp4">
                 </video>
-                <div class="rounded text-white text-center">
+                <div class="p2 rounded text-white text-center">
                     <div id="clock-b" class="countdown countdown-circles d-flex flex-wrap justify-content-center pt-4"></div>
                 </div>
             </div>
@@ -52,12 +78,12 @@
 
     <div class="container text-center" id="balikturismo" display="none">
 
-        <div class="row bg-secondary slider">
+        <div class="row bg-warning slider">
             <div class="offset-md-2 col-md-8 my-4">
                 <div id="demo" class="carousel slide" data-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                            <img class="img-thumbnail" src="http://localhost:8000/storage/Slider/QtFfKkBnObrWKPLZMXskSGIfzVuj1iQdEB8PXRPl.png" class="d-block img-fluid" alt="Los Angeles" width="800" height="500">
+                            <img class="img-thumbnail" src="{{asset('temp_img/balikturismo.jpg')}}" class="d-block img-fluid" alt="Los Angeles" width="800" height="500">
                         </div>
                     </div>
                     <a class="carousel-control-prev" href="#demo" data-slide="prev">
@@ -73,60 +99,131 @@
 
 
         <div class="row">
-            <div class="col-md-4 py-3 border-right border-bottom">
-                <img class="rounded" src="{{asset('temp_img/goldberry.jpeg')}}" alt="" width="170" height="200">                
+            <div class="container2 col-md-4 py-5 border-right border-bottom">
+                <img class="p2 rounded" src="{{asset('temp_logo/goldberry.jpg')}}" alt="" width="170" height="70">
+                <div class="overlay">
+                    <div class="text m-4 p-4">
+                        <button class="btn btn-primary"><span class="text-bold">BROWSE</span></button><br><br>
+                        <a href="http://www.mactan.goldberrysuites.com/" target="_blank"><button class="btn btn-primary"><span class="text-bold">VISIT</span></button></a>
+                    </div>
+                </div>                
             </div>
 
-            <div class="col-md-4 py-3 border-left border-bottom border-right">
-                <img class="rounded"  src="{{asset('temp_img/pacificceburesort.jpeg')}}" alt="" width="170" height="200">
+            <div class="container2 col-md-4 py-5 border-left border-bottom border-right">
+                <img class="p2 rounded"  src="{{asset('temp_logo/pacificceburesort.jpg')}}" alt="" width="170" height="100">
+                <div class="overlay">
+                    <div class="text m-4 p-4">
+                        <button class="btn btn-primary"><span class="text-bold">BROWSE</span></button><br><br>
+                        <a href="http://pacificcebu-resort.com/" target="_blank"><button class="btn btn-primary"><span class="text-bold">VISIT</span></button></a>
+                    </div>
+                </div>
             </div>
 
-            <div class="col-md-4 py-3 border-left border-bottom">
-                <img class="rounded"  src="{{asset('temp_img/savoy.jpeg')}}" alt="" width="170" height="200">
+            <div class="container2 col-md-4 py-5 border-left border-bottom">
+                <img class="p2 rounded"  src="{{asset('temp_logo/savoy.png')}}" alt="" width="150" height="100">
+                <div class="overlay">
+                    <div class="text m-4 p-4">
+                        <button class="btn btn-primary"><span class="text-bold">BROWSE</span></button><br><br>
+                        <a href="https://savoyhotelmactan.com.ph/home/" target="_blank"><button class="btn btn-primary"><span class="text-bold">VISIT</span></button></a>
+
+                    </div>
+                </div>
             </div>
         </div>
         <div class="row">
-            <div class="col-md-4 py-3 border-right border-bottom">
-                <img class="rounded"  src="{{asset('temp_img/whitesands.jpeg')}}" alt="" width="170" height="200">
+            <div class="container2 col-md-4 py-5 border-right border-bottom">
+                <img class="p2 rounded"  src="{{asset('temp_logo/whitesands.jpg')}}" alt="" width="120" height="100">
+                <div class="overlay">
+                    <div class="text m-4 p-4">
+                        <button class="btn btn-primary"><span class="text-bold">BROWSE</span></button><br><br>
+                        <a href="https://www.facebook.com/cebuwhitesandsresortandspa/" target="_blank"><button class="btn btn-primary"><span class="text-bold">VISIT</span></button></a>
+                    </div>
+                </div>
             </div>
 
-            <div class="col-md-4 py-3 border-left border-bottom border-right">
-                <img class="rounded"  src="{{asset('temp_img/jparkisland.jpeg')}}" alt="" width="170" height="200">
+            <div class="container2 col-md-4 py-5 border-left border-bottom border-right">
+                <img class="p2 rounded"  src="{{asset('temp_logo/jparkisland.png')}}" alt="" width="170" height="70">
+                <div class="overlay">
+                    <div class="text m-4 p-4">
+                        <button class="btn btn-primary"><span class="text-bold">BROWSE</span></button><br><br>
+                        <a href="https://www.jparkislandresort.com/" target="_blank"><button class="btn btn-primary"><span class="text-bold">VISIT</span></button></a>
+                    </div>
+                </div>
             </div>
 
-            <div class="col-md-4 py-3 border-left border-bottom">
-                <img class="rounded"  src="{{asset('temp_img/beresort.jpeg')}}" alt="" width="170" height="200">
+            <div class="container2 col-md-4 py-5 border-left border-bottom">
+                <img class="p2 rounded"  src="{{asset('temp_logo/beresort.jpg')}}" alt="" width="120" height="100">
+                <div class="overlay">
+                    <div class="text m-4 p-4">
+                        <button class="btn btn-primary"><span class="text-bold">BROWSE</span></button><br><br>
+                        <a href="https://beresortmactan.com/" target="_blank"><button class="btn btn-primary"><span class="text-bold">VISIT</span></button></a>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="row">
-            <div class="col-md-4 py-3 border-right border-bottom">
-                <img class="rounded"  src="{{asset('temp_img/crimson.jpeg')}}" alt="" width="170" height="200">
+            <div class="container2 col-md-4 py-5 border-right border-bottom">
+                <img class="p2 rounded"  src="{{asset('temp_logo/crimson.png')}}" alt="" width="170" height="100">
+                <div class="overlay">
+                    <div class="text m-4 p-4">
+                        <button class="btn btn-primary"><span class="text-bold">BROWSE</span></button><br><br>
+                        <a href="https://crimsonhotel.com/boracay/offers?type=room_offers" target="_blank"><button class="btn btn-primary"><span class="text-bold">VISIT</span></button></a>
+                    </div>
+                </div>
             </div>
 
-            <div class="col-md-4 py-3 border-left border-bottom border-right">
-                <img  class="rounded" src="{{asset('temp_img/bluewater.jpeg')}}" alt="" width="180" height="200">
+            <div class="container2 col-md-4 py-5 border-left border-bottom border-right">
+                <img  class="p2 rounded" src="{{asset('temp_logo/bluewater.png')}}" alt="" width="180" height="100">
+                <div class="overlay">
+                    <div class="text m-4 p-4">
+                        <button class="btn btn-primary"><span class="text-bold">BROWSE</span></button><br><br>
+                        <a href="http://www.bluewatermaribago.com.ph/" target="_blank"><button class="btn btn-primary"><span class="text-bold">VISIT</span></button></a>
+                    </div>
+                </div>
             </div>
 
-            <div class="col-md-4 py-3 border-left border-bottom">
-                <img  class="rounded" src="{{asset('temp_img/waterfront.jpeg')}}" alt="" width="170" height="200">
+            <div class="container2 col-md-4 py-5 border-left border-bottom">
+                <img  class="p2 rounded" src="{{asset('temp_logo/waterfront.png')}}" alt="" width="170" height="100">
+                <div class="overlay">
+                    <div class="text m-4 p-4">
+                        <button class="btn btn-primary"><span class="text-bold">BROWSE</span></button><br><br>
+                        <a href="https://www.waterfronthotels.com.ph/" target="_blank"><button class="btn btn-primary"><span class="text-bold">VISIT</span></button></a>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="row">
-            <div class="col-md-4 py-3 border-right border-bottom">
-                <img class="rounded"  src="{{asset('temp_img/tambuli.jpeg')}}" alt="" width="170" height="200">
+            <div class="container2 col-md-4 py-5 border-right border-bottom">
+                <img class="p2 rounded"  src="{{asset('temp_logo/tambuli.png')}}" alt="" width="170" height="120">
+                <div class="overlay">
+                    <div class="text m-4 p-4">
+                        <button class="btn btn-primary"><span class="text-bold">BROWSE</span></button><br><br>
+                        <a href="https://tambuliseasideresortandspa.com/" target="_blank"><button class="btn btn-primary"><span class="text-bold">VISIT</span></button></a>
+                    </div>
+                </div>
             </div>
 
-            <div class="col-md-4 py-3 border-left border-bottom border-right">
-                <img  class="rounded" src="{{asset('temp_img/costabella1.jpg')}}" alt="" width="180" height="200">
+            <div class="container2 col-md-4 py-5 border-left border-bottom border-right">
+                <img  class="p2 rounded" src="{{asset('temp_logo/costabella.png')}}" alt="" width="180" height="80">
+                <div class="overlay">
+                    <div class="text m-4 p-4">
+                        <button class="btn btn-primary"><span class="text-bold">BROWSE</span></button><br><br>
+                        <a href="https://costabellaresort.com/" target="_blank"><button class="btn btn-primary"><span class="text-bold">VISIT</span></button></a>
+                    </div>
+                </div>
             </div>
 
-            <div class="col-md-4 py-3 border-left border-bottom">
-                <img  class="rounded" src="{{asset('temp_img/egi1.jpg')}}" alt="" width="170" height="200">
+            <div class="container2 col-md-4 py-5 border-left border-bottom">
+                <img  class="p2 rounded" src="{{asset('temp_logo/egi.jpg')}}" alt="" width="150" height="100">
+                <div class="overlay">
+                    <div class="text m-4 p-4">
+                        <button class="btn btn-primary"><span class="text-bold">BROWSE</span></button><br><br>
+                        <a href="https://www.egiresortandhotel.com/" target="_blank"><button class="btn btn-primary"><span class="text-bold">VISIT</span></button></a>
+                    </div>
+                </div>  
             </div>
         </div>
     </div>
-
-
     @push('scripts')
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.countdown/2.2.0/jquery.countdown.min.js"></script>
         <script>
