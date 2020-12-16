0689fe053e0824102c0a6e9a9ec17cd59e0106ff
brix <brix.does@gmail.com>
2020-12-16 15:39:45 +0800

    changes a

--- resources/views/client/transparency.blade.php
+++ resources/views/client/transparency.blade.php
@@ -40,10 +40,10 @@
                                   <ul>
                                     @foreach ($quart as $fdpdocs)
                                       <li>
-                                        <a href="" class="text-decoration-none" data-toggle="modal" data-target="#{{ str_replace($specials, '', $fdpdocs->title)  }}{{ $key_year }}{{$fdpdocs->type}}"><h5 class="text-uppercase">{{ $fdpdocs->title }}</h5></a>
+                                        <a href="" class="text-decoration-none" data-toggle="modal" data-target="#{{ str_replace($specials, '', $fdpdocs->title.$key_year.$fdpdocs->type)}}"><h5 class="text-uppercase">{{ $fdpdocs->title }}</h5></a>
                                       </li>                          
                                         {{-- MODAL --}}
-                                        <div class="modal fade bd-example-modal-xl" id="{{ str_replace($specials, '', $fdpdocs->title) }}{{ $key_year }}{{$fdpdocs->type}}" tabindex="-1" role="dialog" aria-labelledby="{{ $fdpdocs->id }}" aria-hidden="true">
+                                        <div class="modal fade bd-example-modal-xl" id="{{ str_replace($specials, '', $fdpdocs->title.$key_year.$fdpdocs->type) }}" tabindex="-1" role="dialog" aria-labelledby="{{ $fdpdocs->id }}" aria-hidden="true">
                                           <div class="modal-dialog modal-xl" role="document">
                                             <div class="modal-content">
                                               <div class="modal-header">
