ebdd1365c7b5378083be5def9d3f2fce7d82e099
brix <brix.does@gmail.com>
2021-02-03 12:56:40 +0800

    facebook chat plugin testing

--- resources/views/client/layout/master.blade.php
+++ resources/views/client/layout/master.blade.php
@@ -28,6 +28,31 @@
         @stack('css')
     </head>
     <body id="page-top">
+      <!-- Load Facebook SDK for JavaScript -->
+      <div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v9.0'
+          });
+        };
+
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+      <!-- Your Chat Plugin code -->
+      <div class="fb-customerchat"
+        attribution="setup_tool"
+        page_id="105912244829200"
+        logged_in_greeting="Hi! How can we help you?"
+        logged_out_greeting="Hi! How can we help you?">
+      </div>
         <div id="wrapper">
           <div id="content-wrapper" class="d-flex flex-column">
             @include('client.layout.nav')
