335b5f892aad6a06b09f6fa338c9d57c334a18ea
brix <brix.does@gmail.com>
2022-06-24 08:26:38 +0800

    nav bar link

--- resources/views/clientv2/layout/nav.blade.php
+++ resources/views/clientv2/layout/nav.blade.php
@@ -2,7 +2,7 @@
 <nav class="nav-menu d-none d-lg-block">
   <ul>
     <li class="active"><a href="{{ route('client.home') }}">Home</a></li>
-    <li class="drop-down"><a href="">Online Services</a>
+    <li class="drop-down"><a>Online Services</a>
       <ul>
         <li><a href="https://online.lapulapucity.gov.ph">New/Renew Business Permit</a></li>
         <li><a href="{{ route('client.safetyseal') }}">Request Establishment Safety Seal</a></li>
@@ -10,13 +10,13 @@
         <li><a href="#">Cedula</a></li>
         <li><a href="#">Tax Decleration</a></li>
         <li><a href="#">HR Online Requests</a></li> --}}
-      </ul>
+      </ul> 
     </li>
-    <li class="drop-down"><a href="">Transparency</a>
+    <li class="drop-down"><a>Transparency</a>
       <ul>
         <li><a href="{{ route('client.transparency') }}">Full Disclosure Data</a></li>
         <li><a href="{{ route('client.eo') }}">Executive Order</a></li>
-        <li><a href="#">Citizen's Chart</a></li>
+        {{-- <li><a href="#">Citizen's Chart</a></li> --}}
       </ul>
     </li>
     <li><a href="{{ route('client.downloadables') }}">Downloadables</a></li>
